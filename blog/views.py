b227be2e6253f153732771848fb318ccaff7bb7f
permin0ff <perminoff-ilya@yandex.ru>
2023-05-20 10:37:55 +0300

    Initial commit

---
+++ blog/views.py
@@ -0,0 +1,17 @@
+from .models import Post
+from django.shortcuts import render, get_object_or_404
+
+
+def post_list(request):
+    posts = Post.published.all()
+    return render(request,
+                  'blog/post/list.html',
+                  {'posts': posts})
+
+
+def post_detail(request, id):
+    post = get_object_or_404(Post,
+                             id=id, status=Post.Status.PUBLISHED)
+    return render(request,
+                  'blog/post/detail.html',
+                  {'post': post})
